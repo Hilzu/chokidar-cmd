dbd835891029fe514f0122a2c34e21db5cdb7a2f
Santeri Hiltunen <hilzu@reaktor.fi>
2015-03-30 21:53:58 +0300

    Migrate to a standard code style and test for it

--- cmd.js
+++ cmd.js
@@ -1,68 +1,68 @@
 #!/usr/bin/env node
-"use strict";
+'use strict'; // eslint-disable-line
 
 var chokidar = require('chokidar')
-  , child = require('child_process')
-  , argv = require('yargs')
-    .usage('Usage: chokidar-cmd -c "command" -t file-or-dir')
-    .command('chokidar-cmd', 'Watch directory or file for changes and run given command')
-    .example('chokidar-cmd -c "npm run less" -t src/styles', 'Run less build on changes to styles')
-    .example('chokidar-cmd -c "npm run less" -t src/styles -t ext/styles', 'Run less build on changes to either styles directory')
-    .options({
-      command: {
-        demand: true,
-        type: 'string',
-        alias: 'c',
-        describe: 'Command to run on file changes'
-      },
-      target: {
-        demand: true,
-        type: 'array',
-        alias: 't',
-        describe: 'Target file path, directory and its contents or glob pattern to watch. You can provide ' +
-          'several targets using several target flags.'
-      },
-      verbose: {
-        type: 'boolean',
-        alias: 'v',
-        describe: 'Show verbose output'
-      },
-      quiet: {
-        type: 'boolean',
-        alias: 'q',
-        describe: 'Silence normal output'
-      },
-      initial: {
-        type: 'boolean',
-        describe: 'Run command immediately after initial scan (when chokidar is ready)'
-      }
-    })
-    .help('help')
-    .alias('help', 'h')
-    .version(function() { return require('./package').version })
-    .argv
-  , watcher = chokidar.watch(argv.target, { persistent: true })
-  , run = runner(argv.command)
+var child = require('child_process')
+var argv = require('yargs')
+  .usage('Usage: chokidar-cmd -c "command" -t file-or-dir')
+  .command('chokidar-cmd', 'Watch directory or file for changes and run given command')
+  .example('chokidar-cmd -c "npm run less" -t src/styles', 'Run less build on changes to styles')
+  .example('chokidar-cmd -c "npm run less" -t src/styles -t ext/styles', 'Run less build on changes to either styles directory')
+  .options({
+    command: {
+      demand: true,
+      type: 'string',
+      alias: 'c',
+      describe: 'Command to run on file changes'
+    },
+    target: {
+      demand: true,
+      type: 'array',
+      alias: 't',
+      describe: 'Target file path, directory and its contents or glob pattern to watch. You can provide ' +
+      'several targets using several target flags.'
+    },
+    verbose: {
+      type: 'boolean',
+      alias: 'v',
+      describe: 'Show verbose output'
+    },
+    quiet: {
+      type: 'boolean',
+      alias: 'q',
+      describe: 'Silence normal output'
+    },
+    initial: {
+      type: 'boolean',
+      describe: 'Run command immediately after initial scan (when chokidar is ready)'
+    }
+  })
+  .help('help')
+  .alias('help', 'h')
+  .version(function () { return require('./package').version })
+  .argv
+var watcher = chokidar.watch(argv.target, { persistent: true })
+var run = runner(argv.command)
 
 watcher
-  .on('error', function(err) {
+  .on('error', function (err) {
     logError(err)
     process.exit(1)
   })
-  .on('ready', function() { if (argv.initial) run('initial scan run') })
+  .on('ready', function () { if (argv.initial) run('initial scan run') })
   .on('change', run)
 
 log('Watching "' + argv.target.join('", "') + '" and running command "' + argv.command + '" on changes')
 
-function runner(command) {
+function runner (command) {
   var running = false
 
-  return function(path) {
+  return function (path) {
     verboseLog('Path "' + path + '" changed')
     if (running) return
     running = true
     verboseLog('Executing command: ' + command)
-    execAsync(command, function(err, output) {
+    execAsync(command, function (err, output) {
       if (err) logError(err)
       else verboseLog('Command "' + command + '" completed successfully')
 
@@ -71,31 +71,31 @@ function runner(command) {
   }
 }
 
-function log(msg) {
+function log (msg) {
   if (!argv.quiet) console.log('[chokidar-cmd]', msg)
 }
 
-function logError(err) {
+function logError (err) {
   console.error('[chokidar-cmd] ERROR:', err)
 }
 
-function verboseLog(msg) {
+function verboseLog (msg) {
   if (argv.verbose) log(msg)
 }
 
-function execAsync(cmd, callback) {
+function execAsync (cmd, callback) {
   var output = ''
 
-  var c = child.exec(cmd, { env: process.env, maxBuffer: 20 * 1024 * 1024 }, function(err) {
+  var c = child.exec(cmd, { env: process.env, maxBuffer: 20 * 1024 * 1024 }, function (err) {
     callback(err ? err : null, output)
   })
 
-  c.stdout.on('data', function(data) {
+  c.stdout.on('data', function (data) {
     output += data
     if (!argv.quiet) process.stdout.write(data)
   })
 
-  c.stderr.on('data', function(data) {
+  c.stderr.on('data', function (data) {
     output += data
     process.stderr.write(data)
   })
--- test.js
+++ test.js
@@ -1,3 +1,5 @@
+/*eslint-env node, mocha */
+
 var os = require('os')
 var path = require('path')
 var spawn = require('child_process').spawn
@@ -8,40 +10,40 @@ var tempdir = os.tmpdir()
 var chokidarCmd = path.resolve(__dirname, './cmd.js')
 var p, writeTimer
 
-afterEach(function() {
+afterEach(function () {
   p.kill()
   clearInterval(writeTimer)
 })
 
-test('runs command on file change', function(done) {
+test('runs command on file change', function (done) {
   var target = path.resolve(tempdir, './' + 'test-target-' + Math.random())
   var echoText = target + '-changed-' + Math.random()
 
-  touch.sync(target, function(err) { assert.isNull(err) })
+  touch.sync(target, function (err) { assert.isNull(err) })
 
-  writeTimer = setInterval(function() { appendToFile(target) }, 500)
+  writeTimer = setInterval(function () { appendToFile(target) }, 500)
   p = spawn(chokidarCmd, ['-t', target, '-c', echoCmd(echoText), '-v'], { cwd: tempdir })
 
-  p.stdout.on('data', function(data) {
+  p.stdout.on('data', function (data) {
     process.stdout.write(data)
     if (new RegExp('^' + echoText).test(data.toString())) done()
   })
 
-  p.stderr.on('data', function(data) {
+  p.stderr.on('data', function (data) {
     process.stderr.write(data)
   })
 
-  p.on('error', function(err) {
+  p.on('error', function (err) {
     assert.fail(err)
   })
 })
 
-function echoCmd(msg) {
+function echoCmd (msg) {
   return process.execPath + ' -e "console.log(\'' + msg + '\')"'
 }
 
-function appendToFile(file) {
-  fs.writeFile(file, 'test', function(err) {
+function appendToFile (file) {
+  fs.writeFile(file, 'test', function (err) {
     assert.isNull(err)
   })
 }
